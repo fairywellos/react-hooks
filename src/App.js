e4a766cce1139898a8f8741e320d9d332209b673
fairywellos <fairybutterfly73@gmail.com>
2019-04-09 15:28:18 -0400

    Revert "Initial commit."
    This reverts commit 585a5a4f4447dcfd21df5d5b86d1df1957c30050.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
-import Header from "./component/Header";
-import Home from "./component/Home";
-import Restaurants from "./component/Restaurants";
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Header />
-          <Route exact path="/" component={Home} />
-          <Route path="/restaurants" component={Restaurants} />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     );
   }
 }
