082050c7dbc4f7db8a1cd267eed66adf7e6164f7
fairywellos <fairybutterfly73@gmail.com>
2019-04-10 13:53:52 -0400

    City options.

--- src/component/Restaurants.jsx
+++ src/component/Restaurants.jsx
@@ -1,21 +1,78 @@
 
 
-import React from "react";
+import React, { useState } from "react";
+
+
+const api = "/cities";
 
 
 /**
- * @typedef ResataurantsProps
+ * @typedef RestaurantsProps
  */
 
 /**
  * Displays information about available restaurants.
- * @param {ResataurantsProps} props
+ * @param {RestaurantsProps} props
  * @returns {JSX.Element}
  */
 export default props => {
+    const [region, setRegion] = useState("");
+    const [cities, setCities] = useState({});
+    const [city, setCity] = useState("");
+
+
+    const cityChange = evt => {
+        setCity(evt.target.value);
+    };
+
+    const regionChange = evt => {
+        const { value: nextRegionValue } = evt.target;
+        setRegion(nextRegionValue);
+        setCity("");
+
+        if (!cities[nextRegionValue]) {
+            fetchCities(nextRegionValue);
+        }
+    };
+
+    const fetchCities = async cityRegion => {
+        const response = await fetch(`${api}?filter[state]=${cityRegion}`);
+        const body = await response.json();
+        setCities({ ...cities, [cityRegion]: body.data });
+    };
+
+    let cityOptions = null;
+    if (region && cities && cities[region]) {
+        cityOptions = (
+            <>
+                <option value="">Choose a city</option>
+                {cities[region].map(x => (<option key={x.name} value={x.name}>{x.name}</option>))}
+            </>
+        );
+    }
+
     return (
         <div className="restaurants">
-            <h1>restaurants</h1>
+            <h2 className="page-header">Restaurants</h2>
+
+            <form className="form">
+                <div className="form-group">
+                    <label>State</label>
+                    <select onChange={regionChange} value={region}>
+                        <option value="">Choose a state</option>
+                        <option value="IL">Illinois</option>
+                        <option value="MI">Michigan</option>
+                        <option value="WI">Wisconsin</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label>City</label>
+                    <select disabled={!region} onChange={cityChange} value={city}>
+                        {cityOptions}
+                    </select>
+                </div>
+            </form>
+
         </div>
     );
 }
