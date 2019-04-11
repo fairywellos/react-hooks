252e1324bff509a9172cbbb711ebeeda55cf60b8
fairywellos <fairybutterfly73@gmail.com>
2019-04-11 00:07:37 -0400

    List of restaurants with details buttons displayed.

--- src/component/RestaurantListItem.js
+++ src/component/RestaurantListItem.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { constants } from "../constants";
 
  /**
   * Display restaurant information in a list format.
   * @returns {JSX.Element}
   */
 function RestaurantListItem (props) {
-    const { address, name, thumbnail } = props;
+    const { address, name, selected, slug, thumbnail } = props;
+
+    const detailsClickHandler = () => {
+        selected(props);
+    }
+
     return (
         <div className="restaurant">
             <img src={thumbnail} alt={name} height="100" width="100" />
@@ -14,6 +21,11 @@ function RestaurantListItem (props) {
             <div className="address">
                 {address.street}<br />{address.city}, {address.state} {address.zip}
             </div>
+            <div className="hours-price">
+                $$$<br />Hours: M-F 10am-11pm<span className="open-now">Open Now</span>
+            </div>
+            <Link className="btn" onClick={detailsClickHandler} to={`${constants.pageRestaurants}/${slug}`}>Details</Link>
+            <br />
         </div>
     )
 };
@@ -26,6 +38,7 @@ RestaurantListItem.propTypes = {
         zip: PropTypes.string.isRequired
     }).isRequired,
     name: PropTypes.string.isRequired,
+    selected: PropTypes.func,
     slug: PropTypes.string.isRequired,
     thumbnail: PropTypes.string.isRequired
 };
--- src/component/Restaurants.js
+++ src/component/Restaurants.js
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import { RestaurantListItem } from "./RestaurantListItem";
+
+const DEFAULT_SELECT_VALUE = "";
+
+/**
+ * Information about the restaurants in a city.
+ * @typedef {Object.<string, any[]>} RestaurantCityData
+ */
+
+/**
+ * Information about restaurants in a region's cities.
+ * @typedef {Object.<string, RestaurantCityData>} RestaurantData
+ */
 
 /**
  * Displays information about available restaurants.
@@ -8,15 +20,15 @@ import { RestaurantListItem } from "./RestaurantListItem";
  */
 function Restaurants (props) {
     const [cities, setCities] = useState({});
-    const [city, setCity] = useState("");
-    const [region, setRegion] = useState("");
+    const [city, setCity] = useState(DEFAULT_SELECT_VALUE);
+    const [region, setRegion] = useState(DEFAULT_SELECT_VALUE);
+    /** @type {[RestaurantData, (value: RestaurantData) => void]} */
     const [restaurants, setRestaurants] = useState({});
 
     const cityChange = evt => {
         const { value: nextCityValue } = evt.target;
         setCity(nextCityValue);
 
-        // Fetch restaurants/
         if (nextCityValue && (!restaurants[region] || !restaurants[region][nextCityValue])) {
             fetchRestaurants(region, nextCityValue);
         }
@@ -25,7 +37,7 @@ function Restaurants (props) {
     const regionChange = evt => {
         const { value: nextRegionValue } = evt.target;
         setRegion(nextRegionValue);
-        setCity("");
+        setCity(DEFAULT_SELECT_VALUE);
 
         if (nextRegionValue && !cities[nextRegionValue]) {
             fetchCities(nextRegionValue);
@@ -42,6 +54,13 @@ function Restaurants (props) {
         setRestaurants({ ...restaurants, [filterRegion]: { [filterCity]: body.data } });
     };
 
+    const restaurantSelected = rProps => {
+        const restaurant = restaurants[region][city].find(x => x.slug === rProps.slug);
+        if (restaurant) {
+            props.selected(restaurant);
+        }
+    };
+
     let cityOptions = null;
     if (region && cities && cities[region]) {
         cityOptions = (
@@ -54,11 +73,10 @@ function Restaurants (props) {
 
     let restaurantList = null;
     if (restaurants[region] && restaurants[region][city]) {
-        // {"data":[{"name":"Pig Place","slug":"pig-place","images":{"thumbnail":"node_modules/place-my-order-assets/images/1-thumbnail.jpg","owner":"node_modules/place-my-order-assets/images/2-owner.jpg","banner":"node_modules/place-my-order-assets/images/1-banner.jpg"},"menu":{"lunch":[{"name":"Steamed Mussels","price":21.99},{"name":"Gunthorp Chicken","price":21.99},{"name":"Charred Octopus","price":25.99}],"dinner":[{"name":"Crab Pancakes with Sorrel Syrup","price":35.99},{"name":"Truffle Noodles","price":14.99},{"name":"Garlic Fries","price":15.99}]},"address":{"street":"3108 Winchester Ct.","city":"Detroit","state":"MI","zip":"60602"},"_id":"0OPHdlfpfEXxVwaT"},{"name":"Cow Barn","slug":"cow-barn","images":{"thumbnail":"node_modules/place-my-order-assets/images/2-thumbnail.jpg","owner":"node_modules/place-my-order-assets/images/2-owner.jpg","banner":"node_modules/place-my-order-assets/images/4-banner.jpg"},"menu":{"lunch":[{"name":"Spinach Fennel Watercress Ravioli","price":35.99},{"name":"Truffle Noodles","price":14.99},{"name":"Charred Octopus","price":25.99}],"dinner":[{"name":"Herring in Lavender Dill Reduction","price":45.99},{"name":"Chicken with Tomato Carrot Chutney Sauce","price":45.99},{"name":"Garlic Fries","price":15.99}]},"address":{"street":"230 W Kinzie Street","city":"Detroit","state":"MI","zip":"48229"},"_id":"jTEqxWFkp96imHPQ"}]}
         restaurantList = restaurants[region][city].map(x => {
             const { address, images, name, slug } = x;
-            const rliProps = { address, name, slug, thumbnail: images.thumbnail };
-            return (<RestaurantListItem {...rliProps} />);
+            const rliProps = { address, name, selected: restaurantSelected, slug, thumbnail: images.thumbnail };
+            return (<RestaurantListItem key={region + city + slug} {...rliProps} />);
         });
     }
 
@@ -86,6 +104,10 @@ function Restaurants (props) {
         </div>
     );
 }
+
+Restaurants.propTypes = {
+    selected: PropTypes.func.isRequired
+};
 
 export { Restaurants };
 
