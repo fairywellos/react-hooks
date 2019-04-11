21eac28db2edc9950ca9b7fc3ddb240e5dbf8ddc
fairywellos <fairybutterfly73@gmail.com>
2019-04-11 00:06:03 -0400

    Add RestaurantDetail for placing orders.

---
+++ src/component/RestaurantDetail.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+/**
+ * Displays information about available restaurants.
+ * @returns {JSX.Element}
+ */
+function RestaurantDetail(props) {
+    return (
+        <div className="">
+            <h1>restaurant detail</h1>
+        </div>
+    );
+}
+
+export { RestaurantDetail };
