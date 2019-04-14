72aceb03a3a2817ebb7280d55a1fe7d53a7007df
fairywellos <fairybutterfly73@gmail.com>
2019-04-14 17:35:03 -0400

    Finish RestaurantDetail.

--- src/component/RestaurantDetail.js
+++ src/component/RestaurantDetail.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../AppContext";
 
 /**
- * Displays information about available restaurants.
+ * Displays information about a single restaurant.
  * @returns {JSX.Element}
  */
 function RestaurantDetail(props) {
+    /** @type {AppContextValue} */
+    const context = useContext(AppContext);
+    const { city, region, slug } = context.restaurant.current;
+
+    const restaurant = context.restaurant.restaurants[region][city].find(x => x.slug === slug);
+
+    const headerProps = {
+        className: "restaurant-header",
+        style: {
+            backgroundImage: `url(${window.location.origin}/${restaurant.resources.banner}`
+        }
+    };
+
     return (
-        <div className="">
-            <h1>restaurant detail</h1>
-        </div>
+        <>
+            <div {...headerProps}>
+                <div className="background">
+                    <h2>{restaurant.name}</h2>
+                    <div className="address">
+                        {restaurant.address.street}<br />{restaurant.address.city}, ${restaurant.address.state} ${restaurant.address.zip}
+                    </div>
+                    <div className="hours-price">
+                        $$$<br />Hours: M-F 10am-11pm
+                        <span className="open-now">Open Now</span>
+                    </div>
+                    <br />
+                </div>
+            </div>
+            <div className="restaurant-content">
+                <h3>The best food this side of the Mississippi</h3>
+                <p className="description">
+                    <img alt={`The owner of ${restaurant.name}.`} src={"/" + restaurant.resources.owner} />
+                    Description for {restaurant.name}
+                </p>
+                <p className="order-link">
+                    <a className="btn" href={`${restaurant.slug}/order`}>Order from {restaurant.name}</a>
+                </p>
+            </div>
+        </>
     );
 }
 
 export { RestaurantDetail };
+
+/**
+ * @typedef {import('../AppContext').AppContextValue} AppContextValue
+ */
