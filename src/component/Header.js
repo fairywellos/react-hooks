f3ad7c297dc45c9fdd3231cb28f3301daa94c92a
fairywellos <fairybutterfly73@gmail.com>
2019-04-22 16:06:47 -0400

    Fix Header to use match.

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,22 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 /**
  * Displays information about the app and a menu of choices.
  * @returns {JSX.Element}
  */
 function Header (props) {
-    const { pathname } = props.location;
+    const { path: matchPath } = props.match;
 
+    function isActiveClass(itemPath) {
+        return itemPath === matchPath ? "active" : "";
+    }
+    
     return (
         <header>
             <nav>
                 <h1>place-my-order.com</h1>
                 <ul>
-                    <li className={pathname === "/" ? "active" : ""}><Link to="/">Home</Link></li>
-                    <li className={pathname.startsWith("/restaurants") ? "active" : ""}><Link to="/restaurants">Restaurants</Link></li>
-                    <li className={pathname === "/order-history" ? "active" : ""}><Link to="/order-history">Order History</Link></li>
+                    <li className={isActiveClass("/")}><Link to="/">Home</Link></li>
+                    <li className={isActiveClass("/restaurants")}><Link to="/restaurants">Restaurants</Link></li>
+                    <li className={isActiveClass("/order-history")}><Link to="/order-history">Order History</Link></li>
                 </ul>
             </nav>
         </header>
@@ -29,4 +33,4 @@ Header.propTypes = {
     })
 };
 
-export default withRouter(Header);
+export default Header;
